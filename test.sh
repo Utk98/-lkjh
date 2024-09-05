8be62c0d54556e1e1ebc61ad47e3e065301a3460
Utk98 <umesh83768@gmail.com>
2024-09-05 16:22:23 +0530

    Adding files

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 ./chromium-setup.sh;
 ./chrome-driver-setup.sh;
 
-export DRILL_API_KEY="1_01dce51ff20544ff621eae09c725a4eb50d9fea30251c09d4c17764c343ebea4"
+export DRILL_API_KEY="1_ac4ab713366e7fa3dd46f86f62e2f7da3f339db85c2533727c45d18a92e42b1f"
 export DRILL_API_URL="http://localhost:8090/api"
 
 ./gradlew clean :$1:test;
